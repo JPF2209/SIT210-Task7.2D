963576c478fdeddac0883e8a24fb71c629845908
JPF2209 <105408682+JPF2209@users.noreply.github.com>
2024-05-01 13:19:49 +1000

    Update audio_test.py

--- audio_test.py
+++ audio_test.py
@@ -1,19 +1,26 @@
-
+#import libraries
 import speech_recognition as sr
 from gpiozero import LED
 import RPi.GPIO
+#Setting up LED
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 led = LED(14)
 
+#Setting up mic and recognizer
 r = sr.Recognizer()
 mic = sr.Microphone(device_index = 0, sample_rate = 44100, chunk_size = 512)
 
+#While true
 while True:
+	#Listen out for audio
 	with mic as source:
 		audio = r.listen(source)
+	#Turn into string
 	w = r.recognize_google(audio).lower()
+	#If I said yes, turn led on
 	if w == "yes":
 		led.on()
+	#If i said no, turn led off
 	elif w == "no":
 		led.off()
 
